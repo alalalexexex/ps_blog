2c538754c5c2b438eb22eb0d0a685c7edf854546
alalalexexex <alextrainham@gmail.com>
2019-10-01 17:02:05 -0400

    some good improvements

--- server.js
+++ server.js
@@ -2,20 +2,56 @@
 
 const hbs = require('hbs');     
 const express = require('express'); 
+const mongodb = require('mongodb'); 
 const app = express(); 
+const router = express.Router(); 
 const path = require('path'); 
 
 const port = 8080; 
-
+const mongo = mongodb.MongoClient; 
+const mongoUrl = 'mongodb://127.0.0.1:27017'; 
+const dbName = 'ps-blog'; 
 const viewPath = path.join(__dirname, "/templates/views")
 
 app.set('view engine', 'hbs'); 
 app.set('views', viewPath); 
 app.use(express.static("./public")); 
+app.use('/insert-comment', router); 
 
 app.get('', (req, res) => {
-    res.render("index", {}); 
+    var comments = []; 
+
+    mongo.connect(mongoUrl, {useNewUrlParser: true}, function(err, client){
+        if(err) return console.log('error connecting to mongo'); 
+        const documents = client.db(dbName).collection('comments').find();   
+        documents.forEach((doc, err) => {
+            if(err) return console.log("couldn't find document"); 
+            comments.push(doc); 
+            console.log(comments); 
+        }, () => {
+            res.render('', {items: comments}); 
+        }); 
+    }); 
+
 }); 
+
+app.get('/insert-comment', (req, res) => {
+    const comment_Info = {
+        comment: req.query.comment, 
+        username: req.query.username
+    }
+
+    mongo.connect(mongoUrl, function(err, client) {
+        if(err) return console.log('error connecting to mongo'); 
+        const db = client.db(dbName); 
+        db.collection('comments').insertOne(comment_Info, (err, result) => {
+            if(err) return console.log('cannot insert into database'); 
+            console.log('insert worked'); 
+        }); 
+    }); 
+    res.redirect('../'); 
+}); 
+
 
 app.listen(port, () => {
     console.log("app is listening on port 8080"); 
